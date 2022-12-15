c581d552fbfd3bac64657b6a22cdca0e9e6598a8
Peter.Gallagher <Peter.Gallagher@bjss.com>
2022-12-15 15:32:15 +0000

    Moved some magic numbers to fields, added an array that contains names of items that increase in value as they age

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,20 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+
 class GildedRose {
     Item[] items;
 
+    int minAllowedQuality = 0;
+    int maxAllowedQuality = 50;
+
+    ArrayList<String> itemsIncreaseValueAsTheyAge;
+
     public GildedRose(Item[] items) {
         this.items = items;
+        itemsIncreaseValueAsTheyAge = new ArrayList<>();
+        itemsIncreaseValueAsTheyAge.add("Backstage passes to a TAFKAL80ETC concert");
+        itemsIncreaseValueAsTheyAge.add("Aged Brie");
     }
 
     public void updateQuality() {
@@ -14,19 +24,17 @@ class GildedRose {
                 continue;
             }
 
-            if (!qualityDecreasesAsItemAges(item)){
-                changeQualityWithinLimits(item, -1);
-            } else {
-                    changeQualityWithinLimits(item, 1);
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 6) {
-                            changeQualityWithinLimits(item, 2);
-                        } else if (item.sellIn < 11) {
-                            changeQualityWithinLimits(item, 1);
-                        }
-
+            if (qualityIncreasesAsItemAges(item)){
+                changeQualityWithinLimits(item, 1);
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 6) {
+                        changeQualityWithinLimits(item, 2);
+                    } else if (item.sellIn < 11) {
+                        changeQualityWithinLimits(item, 1);
                     }
+                }
+            } else {
+                changeQualityWithinLimits(item, -1);
             }
 
             item.sellIn = item.sellIn - 1;
@@ -56,19 +64,21 @@ class GildedRose {
 
     private void changeQualityWithinLimits(Item item, int amount_to_change){
         item.quality += amount_to_change;
-        if (item.quality < 0){
-            item.quality = 0;
-        } else if (item.quality > 50) {
-            item.quality = 50;
+
+        if (item.quality < minAllowedQuality){
+            item.quality = minAllowedQuality;
+        } else if (item.quality > maxAllowedQuality) {
+            item.quality = maxAllowedQuality;
         }
     }
 
-    private boolean qualityDecreasesAsItemAges(Item item){
-        if (item.name.equals("Aged Brie")){
-            return true;
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            return true;
-        }
-        return false;
+    private boolean qualityIncreasesAsItemAges(Item item){
+//        if (item.name.equals("Aged Brie")){
+//            return true;
+//        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+//            return true;
+//        }
+//        return false;
+        return itemsIncreaseValueAsTheyAge.contains(item.name);
     }
 }
