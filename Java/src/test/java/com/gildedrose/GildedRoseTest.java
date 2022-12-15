eaafc682bb6cd6e7a329b7130ca37e30aca5e490
Peter.Gallagher <Peter.Gallagher@bjss.com>
2022-12-15 11:17:47 +0000

    Changed loop to smart loop

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void sellinShouldDecreaseByOne() {
+        Item[] items = new Item[] { new Item("foo", 5, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    void qualityShouldNotGoBelowZero() {
+        Item[] items = new Item[] { new Item("foo", -2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void sulfurasQualityIseighty() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        items[0].sellIn = -10;
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
     }
 
 }
