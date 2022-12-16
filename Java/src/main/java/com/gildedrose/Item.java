fb246e0738f73a737433162cc8702dcf4d9501e6
Peter.Gallagher <Peter.Gallagher@bjss.com>
2022-12-16 11:31:31 +0000

    Item now has setQualityWithinLimits method

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -8,10 +8,24 @@ public class Item {
 
     public int quality;
 
+    private final int minAllowedQuality = 0;
+
+    private final int maxAllowedQuality = 50;
+
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+    }
+
+    public void setQualityWithinLimits(int amount_to_change){
+        quality += amount_to_change;
+
+        if (quality < minAllowedQuality){
+            quality = minAllowedQuality;
+        } else if (quality > maxAllowedQuality) {
+            quality = maxAllowedQuality;
+        }
     }
 
    @Override
